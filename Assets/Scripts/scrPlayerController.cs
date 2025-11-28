36fd5c987887847442b1eec590f04efd7905868b
Fr3dd3xs777 <fcardona@my.waketech.edu>
2025-11-28 12:55:45 -0500

    Revert "Merge branch 'main' of https://github.com/YoMxrcus/HauntedNight"
    This reverts commit ac2fb36eb1f61c1837afd1883bfeff95ac3b1ef2, reversing
    changes made to 27659bc79e7a23bd2e7bcfeab52db52f54d59090.

--- Assets/Scripts/scrPlayerController.cs
+++ Assets/Scripts/scrPlayerController.cs
@@ -4,7 +4,6 @@ using TMPro;
 using JetBrains.Annotations;
 using System.Collections;
 using NUnit.Framework;
-using Unity.VisualScripting;
 
 public class scrPlayerController : MonoBehaviour
 {
@@ -21,17 +20,14 @@ public class scrPlayerController : MonoBehaviour
     public AudioClip sprintSound;
 
     //Backpack Variables
-    public GameObject rosaryPNG, rosary;
-    public GameObject flashlightPNG, flashlight;
-    public GameObject battery;
-    public GameObject batteryBar;
-    float batteryAmount = 100;
-    public GameObject keyPNG, key;
+    public GameObject backpackBTN;
+    public GameObject testOBJ, testCube;
     public GameObject inventoryPAN;
+
 
     //Pickup Variables
     public Transform playerCameraTransform;
-    private GameObject currentEquippedItem = null; 
+    bool hasTest;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -76,40 +72,27 @@ public class scrPlayerController : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            ExitInventory();
+            inventoryPAN.SetActive(false);
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
     }
-    public void EquipFlashlight()
-    {
-        ExitInventory();
-        PickUpHandler(flashlight, new Vector3(4.5f, 4.5f, 4.5f), Quaternion.Euler(90f, 90f, 90f));
-    }
-    public void EquipKey()
-    {
-        ExitInventory();
-        PickUpHandler(key, new Vector3(4.5f, 4.5f, 4.5f), Quaternion.Euler(90f, 0f, 90f));
-    }
-    void PickUpHandler(GameObject pickup, Vector3 scale, Quaternion rotation)
-    {
-        if (currentEquippedItem != null)
-        {
-            Destroy(currentEquippedItem);
-            currentEquippedItem = null;
-        }
-        GameObject newPickup = Instantiate(pickup);
-        currentEquippedItem = newPickup;
-
-        newPickup.transform.parent = playerCameraTransform;
-        newPickup.transform.localPosition = new Vector3(.5f, -.25f, .75f);
-        newPickup.transform.localScale = scale;
-        newPickup.transform.localRotation = rotation;
-    }
-    void ExitInventory()
-    {
+    public void Equipped()
+    { 
         inventoryPAN.SetActive(false);
         Time.timeScale = 1;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        if (testOBJ & !hasTest)
+        {
+            GameObject testObject = Instantiate(testCube);
+            testObject.transform.parent = playerCameraTransform;
+            testObject.transform.localPosition = new Vector3(.5f, -.25f, .75f);
+            testObject.transform.localScale = new Vector3(.25f, .25f, .25f);
+            testObject.transform.localRotation = Quaternion.identity;
+            hasTest = true;
+        }
     }
     void UpdateData()
     {
@@ -124,22 +107,11 @@ public class scrPlayerController : MonoBehaviour
                 health -= 10;
                 UpdateData();
                 break;
-            case "rosary":
-                rosaryPNG.SetActive(true);
-                Destroy(other.gameObject);
-                break;
-            case "flashlight":
-                flashlightPNG.SetActive(true);
-                Destroy(other.gameObject);
-                break;
-            case "key":
-                keyPNG.SetActive(true);
-                Destroy(other.gameObject);
-                break;
-            case "battery":
-                Destroy(other.gameObject);
-                break;
 
+            case "pickup":
+                testOBJ.SetActive(true);
+                Destroy(other.gameObject);
+                break;
 
         }
 
