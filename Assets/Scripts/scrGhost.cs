b711c7cd38708fe840fea7bebb8de804f50f4384
YoMxrcus <138556131+YoMxrcus@users.noreply.github.com>
2025-12-02 10:12:29 -0500

    Ghost Movement

--- Assets/Scripts/scrGhost.cs
+++ Assets/Scripts/scrGhost.cs
@@ -6,6 +6,8 @@ public class scrGhost : MonoBehaviour
     public float speed = 5f;
     void Start()
     {
+        Physics.IgnoreLayerCollision(6, 7);
+
         GameObject playerObject = GameObject.FindWithTag("Player");
 
         if (playerObject != null)
@@ -17,7 +19,9 @@ public class scrGhost : MonoBehaviour
     {
         if (target != null)
         {
-            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime); transform.LookAt(target);
+            //transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            //Vector3 playerlook = new Vector3(target.position.x, transform.position.y, target.position.z);
+            //transform.LookAt(playerlook);
         }
     }
     void OnTriggerStay(Collider other)
@@ -27,6 +31,9 @@ public class scrGhost : MonoBehaviour
             case "Player":
                 speed = 1;
                 transform.position = Vector3.MoveTowards(transform.position, other.transform.position, speed * Time.deltaTime);
+
+                Vector3 playerlook = new Vector3(target.position.x, transform.position.y, target.position.z);
+                transform.LookAt(playerlook);
                 break;
         }
     }
