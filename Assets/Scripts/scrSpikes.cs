2bca5b3f87f935012a73024a3fd4e39d9a8d06b4
jadenmcphee14-lab <jadenmcphee14@gmail.com>
2025-12-03 10:11:14 -0500

    Spike Moving Script

--- Assets/Scripts/scrSpikes.cs
+++ Assets/Scripts/scrSpikes.cs
@@ -1,8 +1,13 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement; // Needed to change scenes
 
 public class SpikeTrap : MonoBehaviour
 {
+    public Vector3 startPos;
+    public Vector3 endPos;
+    bool endGoal = false;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -16,6 +21,40 @@ public class SpikeTrap : MonoBehaviour
             // Load the death scene (make sure it's added to Build Settings)
            // SceneManager.LoadScene("DeathScene");
         }
+    }
+    private void Start()
+    {
+        transform.position = startPos;
+    }
+    private void Update()
+    {
+        if (transform.position == startPos)
+        {
+            endGoal = false;
+        }
+        else if (transform.position == endPos)
+        {
+            endGoal = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (!endGoal)
+        {
+            GoToEnd();
+        }
+        else
+        {
+            GoToStart();
+        }
+    }
+    void GoToEnd()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, endPos, .25f);
+    }
+    void GoToStart()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, startPos, .25f);
     }
 }
 
