ecfad2ab23f6c328c9f5f3450947ccdcc8b931c5
YoMxrcus <138556131+YoMxrcus@users.noreply.github.com>
2025-12-03 19:19:16 -0500

    Revert "Merge branch 'main' of https://github.com/YoMxrcus/HauntedNight"
    This reverts commit 65f7547f03a0ac021f3a0373336375af6c3fbadc, reversing
    changes made to 58fdadfa7d59033ac465fc4b3f52534b9eadfd79.

--- Assets/Scripts/scrSpikes.cs
+++ Assets/Scripts/scrSpikes.cs
@@ -1,13 +1,8 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement; // Needed to change scenes
 
 public class SpikeTrap : MonoBehaviour
 {
-    public Vector3 startPos;
-    public Vector3 endPos;
-    bool endGoal = false;
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -19,42 +14,8 @@ public class SpikeTrap : MonoBehaviour
             Debug.Log("Player touched spikes and died!");
 
             // Load the death scene (make sure it's added to Build Settings)
-           // SceneManager.LoadScene("DeathScene");
+            SceneManager.LoadScene("DeathScene");
         }
-    }
-    private void Start()
-    {
-        transform.position = startPos;
-    }
-    private void Update()
-    {
-        if (transform.position == startPos)
-        {
-            endGoal = false;
-        }
-        else if (transform.position == endPos)
-        {
-            endGoal = true;
-        }
-    }
-    private void FixedUpdate()
-    {
-        if (!endGoal)
-        {
-            GoToEnd();
-        }
-        else
-        {
-            GoToStart();
-        }
-    }
-    void GoToEnd()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, endPos, .25f);
-    }
-    void GoToStart()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, startPos, .25f);
     }
 }
 
