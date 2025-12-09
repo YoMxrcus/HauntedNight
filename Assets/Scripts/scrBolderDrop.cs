8a3cae3f808f5e54f1ddd843aade5d6eb51174ce
YoMxrcus <138556131+YoMxrcus@users.noreply.github.com>
2025-12-09 16:35:21 -0500

    Bolder spawning plus skybox

---
+++ Assets/Scripts/scrBolderDrop.cs
@@ -0,0 +1,27 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class scrBolderDrop : MonoBehaviour
+{
+    public GameObject bolder;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Player":
+
+                {
+                    Invoke("SpawnBolder", 3);
+                }
+                break;
+        }
+    }
+    public void SpawnBolder()
+    {
+        bolder.SetActive(true);
+    }
+}
