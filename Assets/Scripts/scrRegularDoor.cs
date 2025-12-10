34f0a7218c3ecbffb89d4230054df20b54e6fd20
YoMxrcus <138556131+YoMxrcus@users.noreply.github.com>
2025-12-09 22:01:25 -0500

    Working on Doors in level 2

---
+++ Assets/Scripts/scrRegularDoor.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+// Removed Unity.VisualScripting; if not used, it's cleaner
+
+public class scrRegularDoor : MonoBehaviour
+{
+    // Make these public and assign them in the Unity Inspector
+    public GameObject inventoryPan;
+    public GameObject keyPNG;
+    public GameObject keyPlayer; // Assuming this is the key object in the player's possession
+
+    public bool hasKey = false;
+
+    // Use Awake or Start to initialize things if you must use Find, but Inspector is better
+    void Start()
+    {
+        
+        inventoryPan = GameObject.Find("InventoryPan");
+        keyPNG = GameObject.Find("keyPNG");
+        keyPlayer = GameObject.Find("KeyPlayer");
+}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Safer way to check tags
+        {
+            // Check if references are assigned before using them
+                inventoryPan.SetActive(true);
+                keyPNG.SetActive(true);
+                hasKey = true;
+                if (hasKey)
+            {
+                Destroy(gameObject); // Destroys the door
+                inventoryPan.SetActive(false);
+                keyPNG.SetActive(false);
+                hasKey = false;
+                // if (inventoryPan != null)
+                //     inventoryPan.SetActive(false);
+            }
+        }
+    }
+}
